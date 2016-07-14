59dadf84c364a60a8656ee9211b6e744419e6ee0
root <fengkiejunis@gmail.com>
2016-07-14 16:16:34 +0700

    changes in sidebar and footer

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -8,7 +8,7 @@
         @if (! Auth::guest())
             <div class="user-panel">
                 <div class="pull-left image">
-                    <img src="{{asset('/img/user2-160x160.jpg')}}" class="img-circle" alt="User Image" />
+                    <img src="{{asset('/img/user3-128x128.jpg')}}" class="img-circle" alt="User Image" />
                 </div>
                 <div class="pull-left info">
                     <p>{{ Auth::user()->name }}</p>
@@ -31,17 +31,36 @@
 
         <!-- Sidebar Menu -->
         <ul class="sidebar-menu">
-            <li class="header">{{ trans('adminlte_lang::message.header') }}</li>
-            <!-- Optionally, you can add icons to the links -->
-            <li class="active"><a href="{{ url('home') }}"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.home') }}</span></a></li>
-            <li><a href="#"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.anotherlink') }}</span></a></li>
+
+            <li class="active"><a href="{{ url('home') }}"><i class='fa fa-dashboard'></i> <span>{{ trans('adminlte_lang::message.home') }}</span></a></li>
+
+            <li class="header">{{ trans('message.projects') }}</li>
             <li class="treeview">
-                <a href="#"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.multilevel') }}</span> <i class="fa fa-angle-left pull-right"></i></a>
+                <a href="#"><i class='fa fa-code'></i> <span> Project 1 </span> <i class="fa fa-angle-left pull-right"></i></a>
                 <ul class="treeview-menu">
-                    <li><a href="#">{{ trans('adminlte_lang::message.linklevel2') }}</a></li>
-                    <li><a href="#">{{ trans('adminlte_lang::message.linklevel2') }}</a></li>
+                    <li><a href="#"><i class='fa fa-gears'></i> Settings </a></li>
+                    <li><a href="#"><i class='fa fa-certificate'></i> Consent Screen </a></li>
+                    <li><a href="#"><i class='fa fa-key'></i> Credentials </a></li>
                 </ul>
             </li>
+
+            <li class="treeview">
+                <a href="#"><i class='fa fa-code'></i> <span> Project 2 </span> <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                    <li><a href="#"><i class='fa fa-gears'></i> Settings </a></li>
+                    <li><a href="#"><i class='fa fa-certificate'></i> Consent Screen </a></li>
+                    <li><a href="#"><i class='fa fa-key'></i> Credentials </a></li>
+                </ul>
+            </li>
+
+            <!-- TODO: Make the projects dynamics by using loop -->
+            <!-- Optionally, you can add icons to the links -->
+            
+            <li><a href="#"><i class='fa fa-plus'></i> <span>{{ trans('message.createproj') }}</span></a></li>
+
+            <li><a href="#"><i class='fa fa-gear'></i> <span>{{ trans('message.settings') }}</span></a></li>
+
+
         </ul><!-- /.sidebar-menu -->
     </section>
     <!-- /.sidebar -->
